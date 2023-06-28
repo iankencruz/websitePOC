c3d4c8758ccd6b54c1e22cd8cab56027af665771
Artic13 <126046296+Artic13@users.noreply.github.com>
2023-06-28 10:32:14 +1000

    got graphql to work. the layout of the code is messy. if we get approval they code structure needs to be improved

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,9 +3,13 @@ import ContentBlock from '@/components/ContentBlock'
 import Hero from '@/components/Hero'
 
 
-async function getData(){
-  const { data } = await directus.items('Home').readByQuery();
-	return data;
+// async function getData(){ // this uses the default api
+//   const { data } = await directus.items('Home').readByQuery();
+// 	return data;
+// }
+async function getData(){ // this uses graphql api to retrive data for this page
+  const { data } = await directus.graphql.items('query{Home{status title body hero{id}}}')
+	return data.Home; // the data we need is nested within ({x:{}}) so this call will just send the data needed (x:{})
 }
 // let title = "tesdast"
 // let body = "yolo"
@@ -13,7 +17,9 @@ async function getData(){
 
 
 export default async function Home() {
-  const d = await getData();
+  const data = await getData();
+  console.log(data);
+  // console.log(data.status);
   return (
     <main>
       
@@ -24,9 +30,9 @@ export default async function Home() {
 
 
       <div className='bg-red-500 w-3/4 h-full mx-auto m-12 text-center'>
-        <h1 className='h-20'>Title goes here: <br /> <div className='w-full h-10 bg-white text-black'>{d.title}</div></h1>
-        <p className='h-20'>Content goes here: <br />  <div className='w-full h-10 bg-white text-black'>{d.body}</div></p>
-        <img src={`${directus.url}assets/${d.hero}`} alt="not working" />
+        <h1 className='h-20'>Title goes here: <br /> <div className='w-full h-10 bg-white text-black'>{data.title}</div></h1>
+        <p className='h-20'>Content goes here: <br />  <div className='w-full h-10 bg-white text-black'>{data.body}</div></p>
+        <img src={`${directus.url}assets/${data.hero.id}`} alt="not working" />
       </div>
     </main>
 
