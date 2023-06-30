a5ee6c713b47eeaac5ba82f381c36df6193cdb13
Artic13 <126046296+Artic13@users.noreply.github.com>
2023-06-30 11:54:06 +1000

    still implementing contact, but api side works. just need to sort ui

--- app/src/app/contact/page.tsx
+++ app/src/app/contact/page.tsx
@@ -2,30 +2,86 @@ import ContentBlock from "@/components/ContentBlock";
 import Hero from "@/components/Hero";
 import { Metadata } from "next";
 import directus from "../lib/directus";
+import ContactForm from "@/components/ContactFrom";
+
 
 export const metadata: Metadata = {
 	title: "contact.js",
 	description: "test",
 };
 
-async function getData(){ // this uses graphql api to retrive data for this page
+async function getData() { // this uses graphql api to retrive data for this page
 	const { data } = await directus.graphql.items('query{Contact{isLeft title body hero{id}}}');
-	  //@ts-ignore
+	//@ts-ignore
 	return data.Contact; // the data we need is nested within ({x:{}}) so this call will just send the data needed (x:{})
-  }
+}
+
+
+const handleSubmit = () => {
+	console.log("hello there");
+}
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default async function Page() {
 	const data = await getData();
-	let x = "" + directus.url + "assets/" + data.hero.id  ;
-	return(
-	<>
-		<h1 className="text-red-500 text-center align-middle">
-			Hello, Contact Page! This is inserted to children
-		</h1>
-        <Hero title={data.title} />
+	let x = "" + directus.url + "assets/" + data.hero.id;
+	return (
+		<>
+			<h1 className="text-red-500 text-center align-middle">
+				Hello, Contact Page! This is inserted to children
+			</h1>
+			<Hero title={data.title} />
 
-    {/* Use props to render content on left or right  */}
-		<ContentBlock srcImage={x} textLeft={data.isLeft} textInput={data.body}/>
-	</>)
+			{/* Use props to render content on left or right  */}
+			<ContentBlock srcImage={x} textLeft={data.isLeft} textInput={data.body} />
+			<ContactForm></ContactForm>
+
+
+			{/* <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+				<div className="max-w-md w-full space-y-8">
+					<form className="mt-8 space-y-6" noValidate onSubmit={() => handleSubmit()}>
+						<div className="rounded-md shadow-sm -space-y-px">
+							<div>
+								<label htmlFor="email-address" className="sr-only">
+									Email address
+								</label>
+								<input
+									id="email-address"
+									name="email"
+									type="email"
+									autoComplete="email"
+									required
+									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+									placeholder="Email address"
+								/>
+							</div>
+							<div>
+								<label htmlFor="password" className="sr-only">
+									Password
+								</label>
+								<input
+									id="password"
+									name="password"
+									type="password"
+									autoComplete="current-password"
+									required
+									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+									placeholder="Password"
+								/>
+							</div>
+						</div>
+
+						<div>
+							<button
+								type="submit"
+								className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+							>
+								Sign up
+							</button>
+						</div>
+					</form>
+				</div>
+			</div> */}
+
+		</>)
 }
---
+++ app/src/components/ContactFrom.tsx
@@ -0,0 +1,96 @@
+'use client'
+import directus from "@/app/lib/directus";
+
+async function handleSubmit(e){
+    e.preventDefault(); 
+	console.log("hello there");
+    const { data } =  await directus.graphql.items('mutation{create_ContactUs_item(data:{name: "ff", email: "gg@tt.com", number : "4546", subject :"higg", message: "gg this work1"})}');
+	console.log(data);
+}
+
+export default function ContactForm() {
+
+   
+
+  return (
+    <div className="mx-auto">
+				<h2 className="text-blue mx-auto text-center text-5xl font-bold my-20">
+					Contact us form
+				</h2>
+                <div className="min-h-full flex items-center justify-center py-2 px-4 sm:px-6 lg:px-8">
+				<div className="max-w-md w-full space-y-8">
+					<form className="mt-8 space-y-6" noValidate onSubmit={(e) => handleSubmit(e)}>
+						<div className="rounded-md shadow-sm -space-y-px">
+							<div>
+								<label htmlFor="email-address" className="sr-only">
+									Email address
+								</label>
+								<input
+									id="email-address"
+									name="email"
+									type="email"
+									autoComplete="email"
+									required
+									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+									placeholder="Email address"
+								/>
+							</div>
+                            <div>
+								<label htmlFor="name" className="sr-only">
+									Name
+								</label>
+								<input
+									id="name"
+									name="name"
+									type="name"
+									autoComplete="user-name"
+									required
+									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+									placeholder="Name"
+								/>
+							</div>
+                            <div>
+								<label htmlFor="subject" className="sr-only">
+									subject
+								</label>
+								<input
+									id="subject"
+									name="subject"
+									type="subject"
+									autoComplete="current-subject"
+									required
+									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+									placeholder="Subject"
+								/>
+							</div>
+                            <div>
+								<label htmlFor="message" className="sr-only">
+									Message
+								</label>
+								<input
+									id="message"
+									name="message"
+									type="message"
+									autoComplete="current-message"
+									required
+									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+									placeholder="Message"
+								/>
+							</div>
+						</div>
+
+						<div>
+							<button
+								type="submit"
+								className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+							>
+								Contact
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+	</div>
+    
+  );
+}
